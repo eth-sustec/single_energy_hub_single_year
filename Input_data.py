5f4c4af5e2f0252a78ad6a2fa461a00ccd60f333
Georgios Mavromatidis <mavromatidis.sustec@gmail.com>
2020-06-05 13:59:27 +0200

    Updated input file for carrier_based analysis

--- Input_data.py
+++ Input_data.py
@@ -11,114 +11,89 @@ import pandas as pd
 
 # Defining input values for model sets
 # ====================================
-Number_of_time_steps = 336
-Time = list(range(1, Number_of_time_steps + 1))
-Number_of_buildings = 10
-Solar_pv_inputs = ["PV" + str(b) for b in range(1, Number_of_buildings + 1)]
-Solar_th_inputs = ["ST" + str(b) for b in range(1, Number_of_buildings + 1)]
-Dispatchable_Tech = ["ASHP", "GSHP", "Gas_Boiler", "Bio_Boiler", "Oil_Boiler", "CHP"]
-Inputs = ["Grid"] + Dispatchable_Tech + Solar_pv_inputs + Solar_th_inputs
-Inputs_wo_grid = Inputs.copy()
-Inputs_wo_grid.remove("Grid")
-CHP_Tech = ["CHP"]
-Outputs = ["Heat", "Elec"]
+Number_of_days = 14
+Days = list(range(1, Number_of_days + 1))
+
+Number_of_time_steps = 24
+Time_steps = list(range(1, Number_of_time_steps + 1))
+
+Solar_tech = ["PV", "ST"]
+Dispatchable_tech = ["ASHP", "GSHP", "Gas_Boiler", "Oil_Boiler", "Bio_Boiler", "CHP"]
+Conversion_tech = Dispatchable_tech + Solar_tech
+Storage_tech = ["Thermal_storage_tank", "Battery"]
+Energy_carriers = ["Heat", "Elec", "NatGas", "Oil", "Biomass"]
+Energy_carriers_imp = ["Elec", "NatGas", "Oil", "Biomass"]
+Energy_carriers_exp = ["Elec"]
+Energy_carriers_dem = ["Heat", "Elec"]
 
 # Defining input values for model parameters
 # ==========================================
-Loads = pd.read_excel(
-    "Time_series_inputs.xlsx", usecols="A:B", index_col=None, header=None
+Demands = pd.read_excel(
+    "Time_series_inputs.xlsx", usecols="A:D", index_col=[0,1], header=[0], sheet_name="Loads_solar"
 )  # Read from some Excel/.csv file
-Loads.index = range(1, Number_of_time_steps + 1)
-Loads.columns = ["Heat", "Elec"]
-Loads = Loads.stack().to_dict()
+Demands = Demands.stack().reorder_levels([2,0,1]).to_dict()
 
 Number_of_days = pd.read_excel(
-    "Time_series_inputs.xlsx", usecols="C", index_col=None, header=None
+    "Time_series_inputs.xlsx", usecols="A:B", index_col=0, header=0, sheet_name="Number_of_days"
 )  # Read from some Excel/.csv file
-Number_of_days.index = range(1, Number_of_time_steps + 1)
-Number_of_days.columns = ["Number_of_days"]
 Number_of_days = Number_of_days.to_dict()
 Number_of_days = Number_of_days["Number_of_days"]
 
 P_solar = pd.read_excel(
-    "Time_series_inputs.xlsx", usecols="D:M", index_col=None, header=None
+    "Time_series_inputs.xlsx", usecols="A:B,E", index_col=[0,1], header=[0], sheet_name="Loads_solar"
 )  # Read from some Excel/.csv file
-P_solar.index = range(1, Number_of_time_steps + 1)
-P_solar.columns = [i for i in range(1, Number_of_buildings + 1)]
-P_solar = P_solar.stack().to_dict()
+P_solar = P_solar.to_dict()
+P_solar = P_solar["P_solar"]
 
-Interest_rate = 0.080
+Discount_rate = 0.080
 
 Network_efficiency = {"Heat": 0.90, "Elec": 1.00}
 Network_length = 200
 Network_lifetime = 40
 Network_inv_cost_per_m = 800
 
-Roof_area_per_bldg = [265, 21, 227, 162, 162, 215, 50, 50, 94, 14]
-Roof_area = {key + 1: Roof_area_per_bldg[key] for key in range(0, Number_of_buildings)}
+Roof_area = 1260
 
 
 # Generation technologies
 # -----------------------
-# Operating_costs, Linear_inv_costs, Fixed_inv_costs, Carbon_factors, Lifetime_tech
-gen_tech = {
-    "Grid": [0.256, 0, 0, 0.0095, 20],
-    "PV1": [0.0, 300, 5750, 0, 20],
-    "ST1": [0.0, 1590, 7630, 0, 20],
-    "ASHP": [0.0, 1530, 6830, 0, 20],
-    "GSHP": [0.0, 2170, 9070, 0, 20],
-    "Gas_Boiler": [0.113, 640, 11920, 0.198, 20],
-    "Bio_Boiler": [0.100, 1150, 24940, 0, 20],
-    "Oil_Boiler": [0.106, 540, 15890, 0.265, 20],
-    "CHP": [0.113, 3100, 43450, 0.198, 20],
+# Linear_inv_costs, Fixed_inv_costs, Lifetime_tech
+gen_tech = {    
+    "PV": [300, 5750, 20],
+    "ST": [1590, 7630, 20],
+    "ASHP": [1530, 6830, 20],
+    "GSHP": [2170, 9070, 20],
+    "Gas_Boiler": [640, 11920, 20],
+    "Bio_Boiler": [1150, 24940, 20],
+    "Oil_Boiler": [540, 15890, 20],
+    "CHP": [3100, 43450, 20],
 }
-gen_tech.update(
-    {
-        key: gen_tech["PV1"]
-        for key in ["PV" + str(i) for i in range(2, Number_of_buildings + 1)]
-    }
-)
-gen_tech.update(
-    {
-        key: gen_tech["ST1"]
-        for key in ["ST" + str(i) for i in range(2, Number_of_buildings + 1)]
-    }
-)
 
-Operating_costs = {key: gen_tech[key][0] for key in gen_tech.keys()}
-Linear_inv_costs = {key: gen_tech[key][1] for key in gen_tech.keys()}
-Fixed_inv_costs = {key: gen_tech[key][2] for key in gen_tech.keys()}
-Carbon_factors = {key: gen_tech[key][3] for key in gen_tech.keys()}
-Lifetime_tech = {key: gen_tech[key][4] for key in gen_tech.keys()}
+Linear_inv_costs = {key: gen_tech[key][0] for key in gen_tech.keys()}
+Fixed_inv_costs = {key: gen_tech[key][1] for key in gen_tech.keys()}
+Lifetime_tech = {key: gen_tech[key][2] for key in gen_tech.keys()}
 
-FiT = 0.120
+Export_prices = {'Elec': 0.120}
+Import_prices = {'Elec': 0.256, 'NatGas': 0.113, 'Oil': 0.106, "Biomass": 0.100}
+Carbon_factors_import = {'Elec': 0.0095, 'NatGas': 0.198, 'Oil': 0.265, "Biomass": 0.0}
 
-Cmatrix = {
-    ("Elec", "Grid"): 1.0,
-    ("Elec", "PV1"): 0.15,
-    ("Heat", "ST1"): 0.35,
-    ("Heat", "ASHP"): 3.0,
-    ("Elec", "ASHP"): -1.0,
-    ("Heat", "GSHP"): 4.0,
-    ("Elec", "GSHP"): -1.0,
-    ("Heat", "Gas_Boiler"): 0.9,
-    ("Heat", "Bio_Boiler"): 0.9,
-    ("Heat", "Oil_Boiler"): 0.9,
-    ("Heat", "CHP"): 0.6,
-    ("Elec", "CHP"): 0.3,
+Conv_factor = {
+    ("PV", "Elec"): 0.15,
+    ("ST", "Heat"): 0.35,
+    ("ASHP", "Heat"): 3.0,
+    ("ASHP", "Elec"): -1.0,
+    ("GSHP", "Heat"): 4.0,
+    ("GSHP", "Elec"): -1.0,
+    ("Gas_Boiler", "Heat"): 0.9,
+    ("Gas_Boiler", "NatGas"): -1.0,
+    ("Bio_Boiler", "Heat"): 0.9,
+    ("Bio_Boiler", "Biomass"): -1.0,
+    ("Oil_Boiler", "Heat"): 0.9,
+    ("Oil_Boiler", "Oil"): -1.0,
+    ("CHP", "Heat"): 0.6,
+    ("CHP", "Elec"): 0.3,
+    ("CHP", "NatGas"): -1.0,
 }
-Cmatrix.update(
-    {
-        key: Cmatrix[("Elec", "PV1")]
-        for key in [("Elec", "PV" + str(i)) for i in range(2, Number_of_buildings + 1)]
-    }
-)
-Cmatrix.update(
-    {
-        key: Cmatrix[("Heat", "ST1")]
-        for key in [("Heat", "ST" + str(i)) for i in range(2, Number_of_buildings + 1)]
-    }
-)
 
 Minimum_part_load = {
     "ASHP": 0.0,
@@ -134,8 +109,8 @@ Minimum_part_load = {
 # Linear_stor_costs, Fixed_stor_costs, Storage_max_charge, Storage_max_discharge, Storage_standing_losses, Storage_charging_eff,
 # Storage_discharging_eff, Storage_max_cap, Lifetime_stor
 stor_tech = {
-    "Heat": [12.5, 1685, 0.25, 0.25, 0.01, 0.90, 0.90, 600, 20],
-    "Elec": [2000, 0, 0.25, 0.25, 0.001, 0.90, 0.90, 600, 20],
+    "Thermal_storage_tank": [12.5, 1685, 0.25, 0.25, 0.01, 0.90, 0.90, 600, 20],
+    "Battery": [2000, 0, 0.25, 0.25, 0.001, 0.90, 0.90, 600, 20],
 }
 
 Linear_stor_costs = {key: stor_tech[key][0] for key in stor_tech.keys()}
@@ -147,3 +122,8 @@ Storage_charging_eff = {key: stor_tech[key][5] for key in stor_tech.keys()}
 Storage_discharging_eff = {key: stor_tech[key][6] for key in stor_tech.keys()}
 Storage_max_cap = {key: stor_tech[key][7] for key in stor_tech.keys()}
 Lifetime_stor = {key: stor_tech[key][8] for key in stor_tech.keys()}
+
+Storage_tech_coupling = {
+        ("Thermal_storage_tank","Heat"): 1.0,
+        ("Battery", "Elec"): 1.0
+}
